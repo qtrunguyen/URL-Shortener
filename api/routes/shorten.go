7cba6402139273f6f548bf6bec23f49e48e81f09
Trung Nguyen <86908083+qtrunguyen@users.noreply.github.com>
2023-12-23 21:41:00 +0700

    URL and domain validation

--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -2,6 +2,10 @@ package routes
 
 import (
 	"time"
+
+	"github.com/gofiber/fiber/v2"
+
+	helpers "github.com/qtrunguyen/URL-Shortener/helpers"
 )
 
 type request struct {
@@ -11,9 +15,31 @@ type request struct {
 }
 
 type response struct {
-	URL             string        `json:"url"`
-	CustomShort     string        `json:"short"`
-	Expiry          time.Duration `json:"expiry"`
-	XRateRemaining  int           `json:"rate_limit"`
-	XRateLimitReset time.Duration `json:"rate_limit_reset`
+	URL         string        `json:"url"`
+	CustomShort string        `json:"short"`
+	Expiry      time.Duration `json:"expiry"`
+
+	// XRateRemaining to prevent excessive amount of request
+	XRateRemaining int `json:"rate_limit"`
+
+	// For each user IP, reset rate limit for the time duration of XRateLimitReset
+	XRateLimitReset time.Duration `json:"rate_limit_reset"`
+}
+
+func ShortenURL(context *fiber.Ctx) error {
+	body := new(request)
+
+	if err := context.BodyParser(&body); err != nil {
+		return context.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "cannot parse JSON"})
+	}
+
+	if !govalidator.IsURL(body.URL) {
+		return context.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "invalid URL"})
+	}
+
+	if helpers.RemoveDomainError(body.URL) {
+		return context.Status(fiber.StatusServiceUnavailable).JSON(fiber.Map{"error": "invalid domain"})
+	}
+
+	body.URL = helpers.EnforceHTTP(body.URL)
 }
